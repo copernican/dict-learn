9f506aebe4036f5953da1ec8127ba7ed6866ab7a
Sean Wilson <copernican@gmail.com>
2016-08-07 00:36:06 -0400

    event detection at 75th and 90th SSE percentiles

--- scratch.R
+++ scratch.R
@@ -3,21 +3,25 @@ dat <- read.csv("sse_and_sparsity.csv")
 plot(sort(dat$SSE, decreasing = T))
 summary(dat$SSE)
 
-sf <- stepfun(x = seq_along(dat$SSE) - 1, y = c(0, dat$SSE))
+sf <- stepfun(x = (seq_along(dat$SSE) - 1) * 2, y = c(0, dat$SSE))
 plot(sf, do.points = F, main = NA, cex.lab = 0.75, cex.axis = 0.75,
      xlab = "Time", ylab = "SSE", xaxt = "n")
 
-sec <- seq_along(dat$SSE) * 2 + 10
+z <- (seq_along(dat$SSE) - 1) * 2
+
+sec <- z + 10 
 labs <- c("00:10", 
           paste(sprintf("%02d", sec[sec %% 30 == 0] %/% 60), 
                 c("30", "00"), sep = ":"))
 
-axis(side = 1, at = c(0, sec[sec %% 30 == 0]), 
+axis(side = 1, at = c(0, z[z %% 30 == 0][-1] - 10), 
      labels = labs, cex.axis = 0.75)
 
 eps <- quantile(x = dat$SSE, probs = c(0.75, 0.9))
 abline(h = eps, col = c("red", "blue"), lty = 2:3)
 
-points(x = which(dat$SSE > eps[1]), 
-       y = dat$SSE[which(dat$SSE > eps[1])])
+events.q3 <- which(dat$SSE > eps[1]) * 2 + 10
+events.q3
 
+events.90 <- which(dat$SSE > eps[2]) * 2 + 10
+events.90
